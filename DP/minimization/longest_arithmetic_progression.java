3243d82d07ead9491b6162c9c982413eb9ba01f8
aditya <adityabbsm@gmail.com>
2025-08-05 16:43:13 +0530

    nice question on sum ending at i and then take max of dp

---
+++ DP/minimization/longest_arithmetic_progression.java
@@ -0,0 +1,46 @@
+package minimization;
+import java.util.*;
+public class longest_arithmetic_progression {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int [] arr = new int[n+1];
+        for( int i = 1 ; i <= n; i++ ) {
+            arr[i] = s.nextInt();
+        }
+
+        solve(arr, n);
+    }
+
+    static void solve(int []arr , int n) {
+        int [] dp= new int[n+1];
+        arr[0] = Integer.MIN_VALUE;
+        Arrays.fill(dp, 1);
+        Set<Integer> set = new LinkedHashSet();
+        for( int i : arr ) set.add(i);
+        for( int i =1 ; i < n ; i++ ) {
+            int max = 1;
+            for( int j = i+1 ; j <= n; j++ ) {
+                int temp = 2;
+
+                int d = arr[j]-arr[i];
+                int cnt = 2;
+                while(d*cnt + arr[i] <= arr[n]) {
+                    if(set.contains(d*cnt + arr[i])) {
+                        temp++;cnt++;
+                    }
+                    else{
+                        break;
+                    }
+                    
+                }
+                max = Math.max(max, temp);
+
+            }
+            dp[i] = max;
+        }
+        System.out.println(Arrays.toString(dp));
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+    
+}
